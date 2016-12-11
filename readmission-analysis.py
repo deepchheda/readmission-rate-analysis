9e37ea0fda0b62de361acc6e8c255727faba0603
deepchheda <deep.chheda08@gmail.com>
2016-12-11 16:09:26 -0500

    first final

--- readmission-analysis.py
+++ readmission-analysis.py
@@ -1,10 +1,16 @@
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn import datasets
 from sklearn import svm
 from sklearn.model_selection import cross_val_score
-from parse_data import clean_data
+from sklearn import tree
+from sklearn import model_selection
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import linear_model, preprocessing
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_regression
+from sklearn.feature_selection import RFECV
 
+
+from parse_data import clean_data
 
 FILE = 'data/diabetic_data.csv'
 if __name__ == "__main__":
@@ -15,12 +21,22 @@ if __name__ == "__main__":
     print y.shape
     x_temp = x[:20]
     y_temp = y[:20]
+    parameters = {}
+    normalized_x = preprocessing.normalize(x, norm='l2')
 
-    clf = svm.SVC(kernel='linear', C=1)
-    scores = cross_val_score(clf, x, y, cv=10)
-    print scores
+    estimator_for_feature_selection = linear_model.LogisticRegression(C=1e5, n_jobs=-1)
+    rfecv = RFECV(estimator_for_feature_selection, step=1, cv=5, n_jobs=-1)
+    X_new = rfecv.fit_transform(normalized_x, y)
+    print X_new.shape
 
-
+    filtered_x = SelectKBest(f_regression, k=20).fit_transform(normalized_x, y)
+    # clf = DecisionTreeClassifier(random_state=0)
+    # clf = svm.SVC(kernel='rbf', C=1)
+    # clf = linear_model.LogisticRegression(C=1e5)
+    clf = KNeighborsClassifier(n_neighbors=500)
+    # clf = svm.SVC(kernel='rbf', C=1, cache_size=20000)
+    score = cross_val_score(clf, X_new, y, cv=10, n_jobs=-1)
+    print score
     # below commented code checks if all values in data are floats
     # for r in data:
     #     for val in r:
